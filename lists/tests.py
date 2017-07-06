8e7eb6e276b4e3c61628a26034f173d7077bbf9a
Jessica <[mildrednwonye@gmail.com]>
2017-07-06 12:12:27 +0200

    Refactor home page view to use a template

---
+++ lists/tests.py
@@ -0,0 +1,14 @@
+from django.core.urlresolvers import resolve
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.test import TestCase
+from django.http import  HttpRequest
+
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
